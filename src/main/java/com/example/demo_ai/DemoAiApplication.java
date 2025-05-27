45cf3d61a4c12a6c40c7c217f8161a69f10106b7
Chun <caochun@gmail.com>
2025-05-27 14:33:10 +0800

    update

--- src/main/java/com/example/demo_ai/DemoAiApplication.java
+++ src/main/java/com/example/demo_ai/DemoAiApplication.java
@@ -1,8 +1,5 @@
 package com.example.demo_ai;
 
-import org.springframework.ai.deepseek.DeepSeekChatModel;
-import org.springframework.ai.deepseek.DeepSeekChatOptions;
-import org.springframework.ai.deepseek.api.DeepSeekApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,13 +10,4 @@ public class DemoAiApplication {
 		SpringApplication.run(DemoAiApplication.class, args);
 	}
 
-	// public DeepSeekChatModel deepSeekChatModel() {
-	// 	var deepSeekApi = new DeepSeekApi(System.getenv("DEEPSEEK_API_KEY"));
-
-	// 	var chatModel = new DeepSeekChatModel(deepSeekApi, DeepSeekChatOptions.builder()
-	// 			.withModel(DeepSeekApi.ChatModel.DEEPSEEK_CHAT.getValue())
-	// 			.withTemperature(0.4f)
-	// 			.withMaxTokens(200)
-	// 			.build());
-	// }
 }
